209fbc740cdcdf488a4ecc2a73bcc85388dc36f9
Sarah Birrer <sarah_birrer@hotmail.com>
2024-05-07 11:23:00 +0200

    move obstacles in level

--- jump-dude/src/jump/dude/panel.java
+++ jump-dude/src/jump/dude/panel.java
@@ -7,10 +7,16 @@ package jump.dude;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class panel extends JPanel {
+public class panel extends JPanel implements ActionListener {
     private Graphics2D graphics2d;
+    private int obstacleXOne = 200; // startpoint from obstacle
+    private int obstacleXTwo = 500;
+    private int obstacleXThree = 700;
     
     @Override
     protected void paintComponent(Graphics g) {
@@ -18,6 +24,10 @@ public class panel extends JPanel {
         graphics2d = (Graphics2D) g;       
         
         var windowWidth = super.size().width;
+        setBackground(Color.BLUE);
+        
+        Timer timer = new Timer(80, this);
+        timer.start();
         
         drawGround(windowWidth);
         drawObstacle();
@@ -25,14 +35,35 @@ public class panel extends JPanel {
     
     private void drawGround(int width)
     {
-        graphics2d.drawLine(0, 600, width, 600);        
+        var groundColor = Color.GREEN;
+        graphics2d.setColor(groundColor);
+        graphics2d.fillRect(0, 600, width, 200);
     }
     
     private void drawObstacle()
     {
-        graphics2d.setColor(Color.RED);
-        graphics2d.fillRect(200, 550, 50, 50); // x, y, width, length
-        graphics2d.fillRect(500, 570, 30, 30);
-        graphics2d.fillRect(700, 560, 50, 50);
+        graphics2d.setColor(Color.MAGENTA);
+        graphics2d.fillRect(obstacleXOne, 550, 50, 50); // x, y, width, length
+        graphics2d.fillRect(obstacleXTwo, 570, 30, 30);
+        graphics2d.fillRect(obstacleXThree, 550, 50, 50);
     }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int speed = 2;
+        obstacleXOne -= speed;
+        obstacleXTwo -= speed;
+        obstacleXThree -= speed;
+        
+        if (obstacleXOne <= 0) {
+            obstacleXOne = +1000; // set to right again
+        }
+        if (obstacleXTwo <= 0) {
+            obstacleXTwo = +1000; // set to right again
+        }
+        if (obstacleXThree <= 0) {
+            obstacleXThree = +1000; // set to right again
+        }
+        repaint();
+    }    
 }

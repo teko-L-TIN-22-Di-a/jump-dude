c025d93ad8c7f24bbe051029e79958ec55d7cfd2
Sarah Birrer <sarah_birrer@hotmail.com>
2024-05-07 10:44:38 +0200

    remove comments

--- jump-dude/src/jump/dude/Main.java
+++ jump-dude/src/jump/dude/Main.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package jump.dude;
 
-import java.awt.Graphics2D;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 /**
@@ -25,7 +20,6 @@ public class Main {
         level.show();
         level.setResizable(false);   
         level.setTitle("Jump Dude");
-        level.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        
+        level.setDefaultCloseOperation(DISPOSE_ON_CLOSE);        
     }
 }
